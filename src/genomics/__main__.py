fc8418ddd59ef0ea39d07173a209dc65ba386e21
Tang, Cheng-Yang(Louis) <louis01010100@gmail.com>
2026-07-05 00:58:09 +0800

    Revert "refactor: remove redundant snv-truth subcommand"
    This reverts commit b8f2cf57b595b803ffee030db0361450936f1673.

--- src/genomics/__main__.py
+++ src/genomics/__main__.py
@@ -7,7 +7,7 @@ from pathlib import Path
 
 import polars as pl
 
-from . import acmg, clinvar, dbsnp, vcf, depth, gene, variants, genome
+from . import acmg, clinvar, dbsnp, vcf, depth, truth, gene, variants, genome
 
 __VERSION__ = '0.6.0'
 
@@ -85,6 +85,20 @@ def main():
     #         coordinates_file=Path(args.coordinates_file),
     #         n_threads=args.n_threads,
     #     )
+    elif args.subcommand == 'snv-truth':
+        truth.export_snv_truth(
+            coordinates_file=Path(args.coordinates_file),
+            vcf_files=_load_files(args.vcfs_file, args.vcf_files),
+            depths_file=_new_path(args.depths_file),
+            samples_file=_new_path(args.samples_file),
+            genders_file=_new_path(args.genders_file),
+            genome_file=Path(args.genome_file),
+            min_depth=args.min_depth,
+            chrm_missing_as_homref=args.chrm_missing_as_homref,
+            merge_vcf=args.merge_vcf,
+            output_dir=Path(args.output_dir),
+            n_threads=args.n_threads,
+        )
     else:
         parser.print_help()
         sys.exit(1)
@@ -112,6 +126,7 @@ def config_parsers():
     _config_acmg_parser(parsers.add_parser('acmg'))
     _config_cram_depth_parser(parsers.add_parser('cram-depth'))
     _config_gvcf_depth_parser(parsers.add_parser('gvcf-depth'))
+    _config_snv_truth_parser(parsers.add_parser('snv-truth'))
     _config_gene_parser(parsers.add_parser('export-gene'))
 
     return parser
@@ -172,6 +187,22 @@ def _config_gvcf_depth_parser(parser):
     parser.add_argument('gvcf_files', nargs='*')
 
 
+def _config_snv_truth_parser(parser):
+    parser.add_argument('--coordinates-file', required=True)
+    parser.add_argument('--vcfs-file', required=False)
+    parser.add_argument('--samples-file', required=False)
+    parser.add_argument('--genders-file', required=False)
+    parser.add_argument('--depths-file', required=False)
+    parser.add_argument('--genome-file', required=True)
+    parser.add_argument('--output-dir', required=True)
+    parser.add_argument('--min-depth', type=int, default=2)
+    parser.add_argument('--chrm-missing-as-homref',
+                        action='store_true',
+                        default=False)
+    parser.add_argument('--merge-vcf', action='store_true', default=False)
+    parser.add_argument('--n-threads', type=int, default=1)
+    parser.add_argument('vcf_files', nargs='*')
+
 def _config_gene_parser(parser):
     parser.add_argument('--genes-file', required=True)
     parser.add_argument('--gene-names-file', required=False)
@@ -182,6 +213,19 @@ def _new_path(file_):
         return Path(file_)
     return None
 
+
+def _load_files(manifest_file, files):
+
+    if manifest_file:
+        files = set()
+        with open(manifest_file, 'rt') as fh:
+            next(fh)
+            for line in fh:
+                files.add(Path(line.strip()))
+
+    files = list(files)
+
+    return files
 
 if __name__ == '__main__':
 
